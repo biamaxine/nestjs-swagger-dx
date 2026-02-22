f6988bbd110fb9602acc107465d335a87db7dd8a
bianca.maxine.vieira <bianca.maxine.vieira@gmail.com>
2026-02-22 10:50:56 +0000

    feat: INSTALL DEPENDENCIES
    -- RUN 'pnpm add -D @nestjs/swagger class-validator class-transformer'
    -- RUN 'pnpm approve-builds'
    -- UPDATED package.json
    -- UPDATED src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Swagger DX')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch(console.error);
